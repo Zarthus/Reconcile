59eb6ea15103ae1bc2b61417840535b39f13eb6a
Zarthus <zarthus@zarth.us>
2014-07-19 23:21:03 +0200

    Processing of events.

--- core/irc.py
+++ core/irc.py
@@ -3,6 +3,7 @@ IRC class for irc connections
 """
 
 import socket
+import time
 import re
 
 from ssl import wrap_socket
@@ -47,9 +48,21 @@ class IrcConnection:
 
                 if self.numeric_regex.match(" ".join(words[:2])):
                     # Check if a server numeric is sent, and handle it appropriately.
-                    if self._processIrcNumeric(words[1]):
+                    if self._processIrcNumeric(words[1], data):
                         # _processIrcNumeric returns true if we need to continue, false if we don't
                         continue
+
+                if self.isEvent(words[1]):
+                    # All events share the following syntax:
+                    # :nick!user@host EVENT target [[:]message/params]
+
+                    uinfo = words[0][:1]
+                    event = words[1]
+                    target = words[2]
+                    params = words[3:] if len(words) > 3 else None
+
+                    self._processEvent(uinfo, event, target, params)
+
 
     def send_raw(self, data):
         self.socket.send(data + "\r\n")
@@ -97,6 +110,15 @@ class IrcConnection:
     def ctcp_reply(self, target, ctcpreply):
         self.send_raw("NOTICE {} :\x01{}\x01".format(target, ctcpreply))
 
+    def join(self, channel):
+        # Store joined_channel temporarily - wait for end of /NAMES to confirm we joined this channel.
+        self.joined_channel = channel
+        self.send_raw("JOIN :{}".format(channel))
+
+    def part(self, channel):
+        self.send_raw("PART :{}".format(channel))
+        self.channelmanager.delForNetwork(self.network_name, channel)
+
     def quit(self, message=None):
         if not message:
             message = "{} shutting down.".format(self.currentnick)
@@ -143,6 +165,7 @@ class IrcConnection:
         self.connected = False
 
         self.id = network["id"]
+        self.network_name = network["network_name"]
         self.server = network["server"]
         self.port = network["port"]
         self.ssl = network["ssl"]
@@ -180,7 +203,7 @@ class IrcConnection:
         self.send_raw("USER {} 0 0 :{}".format(self.ident, self.realname))
         self.connected = True
 
-    def _processIrcNumeric(self, numeric):
+    def _processIrcNumeric(self, numeric, data):
         """
         The ircd sends numerics to indicate something is wrong (or right),
         This method will interact with a few of them
@@ -196,7 +219,75 @@ class IrcConnection:
             # No MOTD found or End of MOTD
             if self.password and self.account:
                 self.send_raw("PRIVMSG NickServ :IDENTIFY {} {}".format(self.account, self.password))
-            
+
             self.send_raw("JOIN :" + ",".join(self.channels))
 
+        if numeric == "366":
+            # End of /NAMES
+            if not self.joined_channel:
+                pass
+
+            self.channelmanager.addForNetwork(self.network_name, self.joined_channel)
+            self.joined_channel = None
+
         return False
+
+    def isEvent(self, event):
+        if event in ["PRIVMSG", "NOTICE", "MODE", "JOIN", "PART", "INVITE", "KICK", "QUIT"]:
+            return True
+        return False
+
+    def _processEvent(self, uinfo, event, target, params):
+        nick = uinfo.split("!")[0]
+        user = uinfo.split("@")[0][len(nick) + 1:]
+        host = uinfo.split("@")[1]
+
+
+        # TODO: handle modules ..
+
+    def on_privmsg(self, nick, target, message):
+        if message.startswith("\x01") and message.endswith("\x01"):
+            if message.lstrip("\x01").startswith("ACTION"):
+                self.on_action(nick, target, message.strip("\x01").strip("ACTION"))
+            else:
+                self.on_ctcp(nick, target, message.strip("\x01"))
+                
+    def on_action(self, nick, target, message):
+        pass
+
+    def on_ctcp(self, nick, target, ctcp):
+        if ctcp == "VERSION":
+            self.ctcp_reply(nick, "{} by {} v{} - {} - written in python".format(self.currentnick, 
+                            self.config.getMaintainer(), self.config.getVersion(), self.config.getGithubURL()))
+        
+        if ctcp == "MAINTAINER":
+            self.ctcp_reply(nick, "{}".format(self.config.getMaintainer()))
+        
+        if ctcp == "TIME":
+            self.ctcp_reply(nick, "{}".format(time.strftime("%H:%M:%S - %A %d %B, %Y")))
+         
+        if ctcp == "PING":
+           self.ctcp_reply(nick, "PONG - {}".format(time.ctime()))
+    
+    def on_notice(self, nick, target, message):
+        pass
+
+    def on_mode(self, nick, target, modes):
+        pass
+
+    def on_join(self, nick, target):
+        pass
+
+    def on_part(self, nick, target):
+        pass
+
+    def on_kick(self, nick, knick, reason):
+        if knick == self.currentnick:
+            self.channelmanager.addForNetwork(
+
+    def on_invite(self, nick, channel):
+        if self.invite_join:
+            self.join(channel)
+
+    def on_quit(self, nick, message=None):
+        pass
