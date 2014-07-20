0e244ee910ae803a128f2fcf53ae61ebddf506a7
Zarthus <zarthus@zarth.us>
2014-07-20 13:31:33 +0200

    Improve the validator class -- also typos!

--- tools/validator.py
+++ tools/validator.py
@@ -7,25 +7,31 @@ import re
 
 
 class Validator:
+    def __init__(self):
+        self.valid_username = re.compile(r"~?[\w]{1,16}@")
+        self.valid_nickname = re.compile(r"[^ \|`\[\]\\-_\w\d]{1,32}!")
 
-    def hostmask(self, hostmask, require_nickanme=False):
+    def hostmask(self, hostmask, require_nickname=False):
         """Check if hostmask is a valid hostmask"""
 
         if len(hostmask) > 128:
             # Whilst possible, masks this long are nearly non existant.
             return False
 
-        pattern = r"~?[\w]{1,16}@.*[\.|\da-h:]{1,}"
-        if require_nickname:
-            pattern = r"[\w\d\[\]\\\|]{1,32}!" + pattern
+        hostmask = hostmask.replace(":", "")
 
-        return re.match(pattern, hostmask)
+        if self.valid_username.match(hostmask):
+            if require_nickname:
+                return self.valid_nickname.match(hostmask)
+            return True
+
+        return False
 
     def nickname(self, nick):
         """Check if nick is a valid IRC nickname"""
-        if re.match(r"[^ \|`\[\]\\-_\w\d]", nick):
-            return False
-        return True
+        if self.valid_nickname.match(nick + "!"):
+            return True
+        return False
 
     def channel(self, channel):
         """Check if channel is a valid channelname"""
