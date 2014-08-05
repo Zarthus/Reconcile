a6ba771c0fceb8dea7e9c482163202dfa617690a
Zarthus <buughost@gmail.com>
2014-08-05 21:17:57 +0200

    Cache currency information.

--- modules/currency.py
+++ modules/currency.py
@@ -8,6 +8,7 @@ This is a module that introduces commands to convert currencies.
 from core import moduletemplate
 
 import requests
+import time
 
 
 class CurrencyConverter(moduletemplate.BotModule):
@@ -20,6 +21,10 @@ class CurrencyConverter(moduletemplate.BotModule):
                               self.PRIV_NONE, ["cur", "convert"])
         self.register_command("currencyinfo", "<currency>", "Retrieve informaiton about <currency>.",
                               self.PRIV_NONE, ["curinfo"])
+
+        self.cache = {"convert": {}, "info": {}}
+        self.cache_currency_convert()
+        self.cache_currency_info()
 
     def on_command(self, target, nick, command, commandtext, mod, admin):
         if command == "currency" or command == "cur" or command == "convert":
@@ -57,6 +62,7 @@ class CurrencyConverter(moduletemplate.BotModule):
             currency = commandtext.upper()
             unabbr = self.currency_info(currency)
             if unabbr:
+                # Yes, the wikipedia url *may* be invalid, but I trust that most of these do exist.
                 return self.reply_target(target, nick, "{} information: Unabbreviated '{}', Wikipedia: {}"
                                                        .format(currency, unabbr, "https://en.wikipedia.org/wiki/{}"
                                                                                  .format(unabbr.replace(" ", "_"))))
@@ -64,23 +70,13 @@ class CurrencyConverter(moduletemplate.BotModule):
                 return self.reply_target(target, nick, "Currency '{}' does not exist or is not known."
                                                        .format(currency))
 
-    def currency_convert(self, amount, currency, ocurrency):  # TODO: Caching.
-        api_url = "http://openexchangerates.org/api/latest.json"
-        api_key = self.api_key["open_exchange_rates"]
+    def currency_convert(self, amount, currency, ocurrency):
+        if self.cache["convert"]["age"] + 3600 * 6 > time.time():
+            self.cache_currency_convert()
 
-        payload = {
-            "app_id": api_key
-        }
+        if "rates" in self.cache["convert"]["json"]:
+            json = self.cache["convert"]["json"]
 
-        json = None
-        try:
-            r = requests.get(api_url, params=payload)
-            r.raise_for_status()
-            json = r.json
-        except Exception as e:
-            return str(e)
-
-        if "rates" in json:
             # One of <currency>'s worth (in json["base"] -- default USD)
             currency_one = 0
             ocurrency_one = 0
@@ -96,13 +92,42 @@ class CurrencyConverter(moduletemplate.BotModule):
                 return "Currency '{}' was not found.".format(ocurrency)
 
             curInUsd = currency_one ** -1  # 1 currency in USD
-            print(curInUsd, type(curInUsd), type(amount))
             curInUsd = curInUsd * amount  # Tells us how much USD the amount is worth.
-
             return curInUsd * ocurrency_one
         return False
 
     def currency_info(self, currency):
+        if self.cache["info"]["age"] + 3600 * 6 > time.time():
+            self.cache_currency_info()
+
+        if currency in self.cache["info"]["json"]:
+            return self.cache["info"]["json"][currency]
+        return False
+
+    def cache_currency_convert(self):
+        api_url = "http://openexchangerates.org/api/latest.json"
+        api_key = self.api_key["open_exchange_rates"]
+
+        payload = {
+            "app_id": api_key
+        }
+
+        json = None
+        try:
+            r = requests.get(api_url, params=payload)
+            r.raise_for_status()
+            json = r.json
+        except Exception as e:
+            self.logger.notice("Error occured caching currency conversion: {}".format(str(e)))
+            return False
+
+        self.cache["convert"]["age"] = time.time()
+        self.cache["convert"]["json"] = json
+        self.logger.log_verbose("Cached currency conversions.")
+
+        return True
+
+    def cache_currency_info(self):
         api_url = "http://openexchangerates.org/api/currencies.json"
         api_key = self.api_key["open_exchange_rates"]
 
@@ -116,8 +141,10 @@ class CurrencyConverter(moduletemplate.BotModule):
             r.raise_for_status()
             json = r.json
         except Exception as e:
-            return str(e)
+            self.logger.notice("Error occured caching currency info: {}".format(str(e)))
+            return False
 
-        if currency in json:
-            return json[currency]
-        return False
+        self.cache["info"]["age"] = time.time()
+        self.cache["info"]["json"] = json
+        self.logger.log_verbose("Cached currency information.")
+        return True
