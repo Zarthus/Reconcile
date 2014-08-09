5b62868567cdbbf0a0d311797b908f8f5ddda54a
Zarthus <buughost@gmail.com>
2014-08-09 22:30:53 +0200

    Add debug_flood.py to debug the rate limiter.

--- modules_disabled/debug_command.py
+++ modules_disabled/debug_command.py
@@ -2,7 +2,7 @@
 CmdDebug by Zarthus
 Licensed under MIT
 
-This plugin will notice all params sent by on_command to the user.
+This module will notice all params sent by on_command to the user.
 Do not run this module if you're in spammy channels, use a test instance of the bot instead.
 """
 
@@ -11,4 +11,4 @@ from core import moduletemplate
 
 class CmdDebug(moduletemplate.BotModule):
     def on_command(self, target, nick, command, commandtext, mod, admin):
-        self.reply_notice(target, str([target, nick, command, commandtext, mod, admin]))
+        self.reply_notice(nick, str([target, nick, command, commandtext, mod, admin]))
---
+++ modules_disabled/debug_flood.py
@@ -0,0 +1,39 @@
+"""
+FloodDebug by Zarthus
+Licensed under MIT
+
+This module will flood, allowing you to debug the rate limitation system.
+"""
+
+from core import moduletemplate
+
+import time
+
+
+class FloodDebug(moduletemplate.BotModule):
+    def on_module_load(self):
+        self.register_command("flood_notice", None, "Flood your notice with ten messages.", self.PRIV_ADMIN)
+        self.register_command("flood_channel", None, "Flood the channel with ten messages.", self.PRIV_ADMIN)
+        self.register_command("flood_query", None, "Flood your query with ten messages.", self.PRIV_ADMIN)
+
+        self.last_flood = int(time.time())
+
+    def on_command(self, target, nick, command, commandtext, mod, admin):
+        if admin:
+            if command == "flood_notice":
+                if int(time.time()) > self.last_flood + 5:
+                    self.last_flood = int(time.time())
+                    for i in range(10):
+                        self.reply_notice(nick, "flood_notice(): Message {}".format(i))
+
+            if command == "flood_channel":
+                if int(time.time()) > self.last_flood + 5:
+                    self.last_flood = int(time.time())
+                    for i in range(10):
+                        self.reply_target(target, nick, "flood_channel(): Message {}".format(i))
+
+            if command == "flood_query":
+                if int(time.time()) > self.last_flood + 5:
+                    self.last_flood = int(time.time())
+                    for i in range(10):
+                        self.reply_target(nick, None, "flood_query(): Message {}".format(i))
