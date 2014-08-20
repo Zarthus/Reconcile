b915d3f6336192a3e10d9f52750e0b2dd4b58a6e
Zarthus <buughost@gmail.com>
2014-08-20 22:38:10 +0200

    Remove check.sh - it didn't serve much use and wasn't used.

--- check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script has only been tested (and most likely only works) on linux machines.
-# This script can be used to see if the commit(s) you are about to make will work with travis.
-
-flake8 .
-echo
-python3 travis/test_module.py | tail -4
-
