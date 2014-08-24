5e5289b0d75566f9d2b0d79d2b46754bcb7fe05a
Zarthus <buughost@gmail.com>
2014-08-24 13:03:20 +0200

    Create an ircbot.pid file on startup, remove on bot exit.
    This allows us to monitor easily if a bot process is already running.

--- bot.py
+++ bot.py
@@ -7,9 +7,21 @@ from core import config
 from core import irc
 
 import time
+import os
 
 
 conf = config.Config()
+
+if os.path.isfile("ircbot.pid"):  # There is a pid file, the bot is already running. Stop.
+    conf.logger.error("An ircbot.pid file exists, there is already an instance of the bot running.")
+    conf.logger.error("Please shut down that instance of the bot and run this command again.")
+    import sys
+    sys.exit(1)
+
+f = open("ircbot.pid", "w")
+f.write(str(os.getpid()))
+f.close()
+
 irc_connections = {}
 running = True
 
@@ -27,3 +39,5 @@ while running:
             for c in irc_connections.items():
                 c[1].quit("Shutdown requested.")
             running = False
+
+os.remove("ircbot.pid")  # Remove pid file as it is no longer running.
